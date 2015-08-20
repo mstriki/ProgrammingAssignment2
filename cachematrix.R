7abcf2e57d8a6e73460b48690e39ab3247be76ee
Maria Striki <mstriki@gmail.com>
2015-08-20 00:49:00 -0400

    cachematrix.R - 2nd commit attempt
    Almost done, before adding comments, before elaboration, but program
    runs correctly

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## [Put comments here that describe what your functions do]
+keep <- list(makeCacheMatrix)
 makeCacheMatrix <- function(x = matrix()) {
-
+       m <- NULL      
+       set <- function(y)  {
+              x <<- y
+       #       nrow(x) <<- nrow(y)
+        #      ncol(x) <<- ncol(y)
+              m <<- NULL
+              y
+       }       
+       get <- function() x
+       setInv <- function(solve) m <<- solve
+       getInv <- function() m
+       list (set = set, get = get, setInv = setInv, getInv = getInv)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+       
+        if (i>1) {
+        for (j in 1:(i-1)) {
+             stmt <- identical(keep[[j]]$get(),x$get())
+             if (stmt) {
+                   x <- keep[[j]]
+             }
+        }
+        }
+        m <- x$getInv()
+        if (!is.null(m)) {
+             message("getting cached data")
+             return(m)
+        }
+        
+        keep[[i]] <<- x 
+        i <<- i+1
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInv(m)
+        m
+        message("getting non-cached data")
+        print(data)
 }
